c57d862a8202543684a483d7c9bb6e66cf1198f4
朱 思源 <1240208181@qq.com>
2023-12-05 15:03:42 +0800

    代码生成器的制作第一步

---
+++ generator-maker/src/main/java/com/zsy/maker/Main.java
@@ -0,0 +1,18 @@
+package com.zsy.maker;
+
+//import com.zsy.maker.cli.CommandExecutor;
+
+/**
+ * @author zhusiyuan
+ * @date 2023/11/29
+ * @apiNote
+ */
+public class Main {
+    public static void main(String[] args) {
+        //args = new String[]{"generate", "-l", "-a", "-o"};
+        //args = new String[]{"config"};
+        //args = new String[]{"list"};
+        //CommandExecutor commandExecutor = new CommandExecutor();
+        //commandExecutor.doExecute(args);
+    }
+}
