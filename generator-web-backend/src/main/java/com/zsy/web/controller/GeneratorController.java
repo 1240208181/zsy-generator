4a35b4075a3420e6c4b88ea2972aa695679d73ea
朱 思源 <1240208181@qq.com>
2024-02-03 19:56:00 +0800

    项目上线

--- generator-web-backend/src/main/java/com/zsy/web/controller/GeneratorController.java
+++ generator-web-backend/src/main/java/com/zsy/web/controller/GeneratorController.java
@@ -467,7 +467,7 @@ public class GeneratorController {
         File scriptFile = FileUtil.loopFiles(unzipDistDir, 2, null)
                 .stream()
                 .filter(file -> file.isFile()
-                        && "generator.bat".equals(file.getName()))
+                        && "generator".equals(file.getName()))
                 .findFirst()
                 .orElseThrow(RuntimeException::new);
         // 添加可执行权限
@@ -479,8 +479,12 @@ public class GeneratorController {
 
         // 构造命令
         File scriptDir = scriptFile.getParentFile();
+        // win 系统
+//        String scriptAbsolutePath = scriptFile.getAbsolutePath().replace("\\", "/");
+//        String[] commands = new String[]{scriptAbsolutePath, "json-generate", "--file=" + dataModelFilePath};
+
         // 注意，如果是 mac / linux 系统，要用 "./generator"
-        String scriptAbsolutePath = scriptFile.getAbsolutePath().replace("\\", "/");
+        String scriptAbsolutePath = scriptFile.getAbsolutePath();
         String[] commands = new String[]{scriptAbsolutePath, "json-generate", "--file=" + dataModelFilePath};
 
         // 这里一定要拆分！
